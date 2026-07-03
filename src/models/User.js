63ae579df867d58c26dd629236653f82d2dd58f6
Veto <st4120@utr.edu.mx>
2026-07-03 11:24:31 -0600

    Agrega campos para bloqueo temporal de usuarios

--- src/models/User.js
+++ src/models/User.js
@@ -3,23 +3,29 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
 
     username: {
-
         type: String,
-
         required: true,
-
-        unique: true
-
+        unique: true,
+        trim: true
     },
 
     password: {
-
         type: String,
-
         required: true
+    },
 
+    failedAttempts: {
+        type: Number,
+        default: 0
+    },
+
+    lockUntil: {
+        type: Date,
+        default: null
     }
 
+}, {
+    timestamps: true
 });
 
 module.exports = mongoose.model("User", userSchema);
